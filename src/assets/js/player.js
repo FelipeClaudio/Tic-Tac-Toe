1739fe0730160b10b0552bbb03c7879f666d5827
Felipe Santos <felipesantos956@poli.ufjr.br>
2021-01-30 16:24:29 -0300

    Adds tests for player class.

--- src/assets/js/player.js
+++ src/assets/js/player.js
@@ -1,9 +1,18 @@
 export default class Player {
     constructor(symbol){
-        this.symbol = symbol
+        const upperCaseSymbol = symbol?.toUpperCase()
+        if (this.#IsValidSymbolInput(upperCaseSymbol))
+            this.symbol = upperCaseSymbol
     }
 
     getPlayerSymbol = () => {
         return this.symbol
     }
+
+    #IsValidSymbolInput = (upperCaseSymbol) => {       
+        if (upperCaseSymbol !== "X" && upperCaseSymbol !== "O")
+            throw new TypeError("Invalid player symbol. It should be either 'X' or 'O'.")
+
+        return true
+    } 
 }
