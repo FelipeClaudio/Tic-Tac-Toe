43baf14fd03cb80e40f493933a8a95e77e73e5de
Felipe Santos <felipesantos956@poli.ufjr.br>
2021-01-30 16:52:54 -0300

    Adds unit tests for 'RestartButton' class.

--- src/assets/js/restartButton.js
+++ src/assets/js/restartButton.js
@@ -1,12 +1,21 @@
 export default class RestartButton {
     constructor(handleRestartGameEvent){
-        this.restartGameButton = document.createElement('button')
-        this.restartGameButton.innerHTML = "Restart Game"
-        this.restartGameButton.classList.add('restart-button')
-        this.restartGameButton.onclick = handleRestartGameEvent
+        if(this.#IsFunction(handleRestartGameEvent)){
+            this.restartGameButton = document.createElement('button')
+            this.restartGameButton.innerHTML = "Restart Game"
+            this.restartGameButton.classList.add('restart-button')
+            this.restartGameButton.onclick = handleRestartGameEvent
+        } 
     }
 
     getHtml = () => {
         return this.restartGameButton
     }
+
+    #IsFunction = (argument) => {       
+        if (!argument || {}.toString.call(argument) !== '[object Function]')
+            throw new TypeError("Argument should be a function.")
+
+        return true
+    } 
 }
