7fe581c6e8b4fe0d02f861c0ea742ad3d23de9ab
Felipe Santos <felipesantos956@poli.ufjr.br>
2021-02-11 23:24:40 -0300

    Rebases branch.

--- webpack.config.js
+++ webpack.config.js
@@ -1,58 +1,56 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin')
-const MiniCssExtractPlugin = require('mini-css-extract-plugin')
-const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
-const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin')
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
+const OptimizeCssAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+const path = require("path");
 
-const isDevEnv = process.env.NODE_ENV !== 'production'
+const isDevEnv = process.env.NODE_ENV !== "production";
 
 module.exports = {
-    mode: isDevEnv ? 'development' : 'production',
-    entry: './src/assets/js/index.js',
-    output: {
-        filename: 'game_final.js',
-        path: __dirname + '/public'
-    },
-    devServer: {
-        contentBase: "./public",
-        port: 9123,
-    },
-    optimization: {
-        minimizer: [
-            new UglifyJsPlugin({
-                cache: true,
-                parallel: true,
-                sourceMap: true
-            }),
-            new OptimizeCssAssetsPlugin({
-                sourceMap: true
-            })
-        ]
-    },
-    module: {
-        rules: [
-        {
-            test: /\.m?js$/,
-            exclude: /(node_modules|bower_components)/,
-            use: {
-                loader: 'babel-loader',
-                options: {
-                presets: ['@babel/preset-env']
-                }
-            }
+  mode: isDevEnv ? "development" : "production",
+  entry: path.resolve(__dirname, "./src/assets/js/index.js"),
+  output: {
+    filename: "game_final.js",
+    path: __dirname + "/public",
+  },
+  devServer: {
+    contentBase: "./public",
+    port: 9123,
+  },
+  optimization: {
+    minimizer: [
+      new UglifyJsPlugin({
+        cache: true,
+        parallel: true,
+        sourceMap: true,
+      }),
+      new OptimizeCssAssetsPlugin({
+        sourceMap: true,
+      }),
+    ],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
         },
-        {
-            test: /\.s[ac]ss$/i,
-            use: [
-                MiniCssExtractPlugin.loader,
-                "css-loader",
-                "sass-loader",
-            ],
-        }]
-    },
-    plugins: [
-        new HtmlWebpackPlugin(),
-        new MiniCssExtractPlugin({
-            filename: "game_final.css"
-        })
-    ]
-}
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin(),
+    new MiniCssExtractPlugin({
+      filename: "game_final.css",
+    }),
+  ],
+};
